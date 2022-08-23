00d732a30c5712edd66dcfd4ff83b14c3e471b6d
Professional <sania_38@list.ru>
2022-08-23 22:58:29 +0600

    feat: add footer

---
+++ src/component/main/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './main.css'
+import Auth from "../auth";
+
+const Main = () => {
+    return (
+        <main className='main'>
+            <Auth/>
+        </main>
+    );
+};
+
+export default Main;
