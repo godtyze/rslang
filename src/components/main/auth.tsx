bdec6a552b47a7e9ff392aefea5550a4a992e16c
godtyze <omgfail301@gmail.com>
2022-08-27 21:24:06 +0600

    fix: fix minor bug with register form

--- src/components/main/auth.tsx
+++ src/components/main/auth.tsx
@@ -5,27 +5,25 @@ import RegisterForm from "../UI/registerForm/registerForm";
 
 const Auth: React.FC = () => {
     const [visible, setVisible] = useState<boolean>(true);
+
     const clickHandler = () => {
         setVisible(!visible);
     }
+
     return (
-        <div>
-            {visible
-                ? <MyButton
-                    className={'btn login-button'}
-                    onClick={clickHandler}
-                    visible={visible}>
-                    Вход / Регистрация
-                </MyButton>
-                : <RegisterForm children={<MyButton
-                    className={'btn form-button'}
-                    onClick={clickHandler}
-                    visible={true}>
-                    X
-                </MyButton>}/>
-            }
-        </div>
+      <div>
+          {visible
+            ? <MyButton
+              className={'btn login-button'}
+              onClick={clickHandler}
+              visible={visible}>
+                Вход / Регистрация
+            </MyButton>
+            : <RegisterForm onClick={clickHandler}/>
+          }
+      </div>
     );
 };
 
 export default Auth;
+
