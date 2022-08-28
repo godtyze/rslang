1669df86bd45961c7e78e5774ad0a7966935bdf6
Professional <sania_38@list.ru>
2022-08-29 01:05:55 +0600

    feat: add start game screen

--- src/pages/games/audio-call/audio-call.tsx
+++ src/pages/games/audio-call/audio-call.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import AudioMain from "../../../components/audio-call/audio-main";
+import MyButton from '../../../components/UI/button/button';
+import '../../../components/audio-call/audio-call.css'
 
-const AudioCall: React.FC = () => {
+const AudioCall = () => {
   return (
-    <div>
-      Audio-call
-    </div>
+      <div>
+        <div className='audio-main'>
+        </div>
+        <AudioMain/>
+        <Link to='/'><MyButton className={'game-button'} visible={true}>X</MyButton>
+        </Link>
+      </div>
   );
 };
+
+
 
 export default AudioCall;
