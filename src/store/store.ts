05a18aac658015aec20f8682f62cd90c8c59a11e
godtyze <omgfail301@gmail.com>
2022-10-13 21:42:34 +0600

    remove assets

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,4 @@
-import {combineReducers, configureStore} from "@reduxjs/toolkit";
+  import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import userReducer from './reducers/UserSlice';
 
 const rootReducer = combineReducers({
