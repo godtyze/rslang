4800102d9f921e41c23a4a091322d3746096f763
godtyze <omgfail301@gmail.com>
2022-11-01 19:43:43 +0600

    refactor: relocate functions to utils file

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,17 +1,20 @@
+import moment from "moment/moment";
+import {expireTime} from "../consts/consts";
+
 const audioPlayer = new Audio();
 
 
-export async function playAudio(url: string) {
+export async function playAudio(url: string): Promise<void> {
   audioPlayer.src = url;
   audioPlayer.load();
   await audioPlayer.play();
 }
 
-export const getRandomElement = (n: number) => {
+export const getRandomElement = (n: number): number => {
   return Math.floor(Math.random() * n);
 }
 
-export function shuffle(arr: string[]) {
+export function shuffle(arr: string[]): string[] {
   let idx = arr.length, randomIdx;
   while (idx !== 0) {
     randomIdx = Math.floor(Math.random() * idx);
@@ -20,4 +23,13 @@ export function shuffle(arr: string[]) {
       arr[randomIdx], arr[idx]];
   }
   return arr;
+}
+
+export function getErrorMessage (error: unknown): string {
+  if (error instanceof Error) return error.message;
+  return String(error);
+}
+
+export function getNextExpireTime(): string {
+  return moment().add(expireTime, 'hours').toISOString();
 }
