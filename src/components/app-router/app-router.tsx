84f75746f7c0be42a812da0b299aa3f30e87d481
godtyze <omgfail301@gmail.com>
2022-11-07 17:00:53 +0600

    feat: add lazy loading for glossary page

--- src/components/app-router/app-router.tsx
+++ src/components/app-router/app-router.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import Main from "../../pages/main/main";
-import Glossary from "../../pages/glossary/glossary";
 import AudioCall from "../../pages/games/audio-call/audio-call";
 import Sprint from "../../pages/games/sprint/sprint";
 import {Navigate, useRoutes} from "react-router-dom";
+import Loader from "../Loader/loader";
+
+const Glossary = React.lazy(() => import('../../pages/glossary/glossary'));
 
 const AppRouter: React.FC = () => {
   const routes = useRoutes([
     {path: '/', element: <Main/>},
-    {path: '/glossary/:group/:page', element: <Glossary/>},
+    {path: '/glossary/:group/:page', element:
+        <React.Suspense fallback={<div className='loader-wrapper'><Loader/></div>}>
+          <Glossary/>
+        </React.Suspense>
+    },
     {path: '/audio-call', element: <AudioCall/>},
     {path: '/sprint', element: <Sprint/>},
     {path: '*', element: <Navigate to="/" replace />}
