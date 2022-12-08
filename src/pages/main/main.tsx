3db649d81bf37832e454f35229f6b29d7a36c753
godtyze <omgfail301@gmail.com>
2022-12-08 23:34:09 +0600

    feat: remove unused jsx

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import './main.css'
 import '../../App.css'
 import Auth from "./auth";
@@ -8,36 +8,27 @@ import {useAppSelector} from "../../hooks/redux";
 
 
 const Main: React.FC = () => {
-  const {isAuth} = useAppSelector(state => state.userReducer);
-  const [descriptionText, setDescriptionText] = useState<number>(1);
+  const isAuth = useAppSelector(state => state.userReducer.isAuth);
 
   return (
     <div className='App'>
       <header className='main-header'>
         <Auth/>
         {isAuth &&
-            <div className='profile-logo'>
+          <div className='profile-logo'>
             <img className='main-image' src={require('../../assets/png/prof.png')} height='100' width='100'
                  alt='profile-pic'/>
             <span>Профиль</span>
-            </div>
+          </div>
         }
       </header>
       <main className='main'>
-          <Games/>
-          <div className='about'>
-            {descriptionText === 0
-                ? <span>Изучайте иностранные языки вместе с нами!
+        <Games/>
+        <div className='about'>
+            <span>Изучайте иностранные языки вместе с нами!
                 Благодаря нашему приложению познавать новое увлекательно и весело, опробуйте наши миниигры, и убедитесь сами<br/>
-                </span>
-                : <div>
-                  <span>Наша команда:</span>
-                </div>}
-            <div className='btn-wrap'>
-              <button onClick={() => setDescriptionText(0)} className={descriptionText === 0 ? 'authors-button active' : 'authors-button'}></button>
-              <button onClick={() => setDescriptionText(1)} className={descriptionText === 1 ? 'authors-button active' : 'authors-button'}></button>
-            </div>
-          </div>
+            </span>
+        </div>
       </main>
       <Footer/>
     </div>
